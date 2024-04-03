1d07aa6a6108e33ae8de2f4e5354162a76b3605b
hellen kimani <astridlee80@gmail.com>
2024-04-03 14:57:00 +1000

    First Attempt

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("bathe my dog");
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork("work from home");
+
+function wrapAdjective (target = "*a hard worker*") {
+    const part1 = "special";
+        return function () {
+             return `You are ${target}!`};
+    };
+const encouragingPromptFunction = wrapAdjective("a hard worker")();
